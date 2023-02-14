fb0face926e5144a7b16c826988c5c93116a89ec
GaoJH <779697959@qq.com>
2023-02-14 21:08:05 +0800

    feat: get douban group info

---
+++ internal/pkg/collect/collect.go
@@ -0,0 +1,52 @@
+package collect
+
+import (
+	"bufio"
+	"crawler/pkg/format"
+	"crawler/pkg/proxy"
+	"fmt"
+	"golang.org/x/text/transform"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+type Fetcher interface {
+	Get(resq *Request) ([]byte, error)
+}
+
+type BrowserFetch struct {
+	Timeout time.Duration
+	Proxy   proxy.ProxyFunc
+}
+
+// 模拟浏览器访问
+func (b *BrowserFetch) Get(request *Request) ([]byte, error) {
+
+	client := &http.Client{
+		Timeout: b.Timeout,
+	}
+	if b.Proxy != nil {
+		transport := http.DefaultTransport.(*http.Transport)
+		transport.Proxy = b.Proxy
+		client.Transport = transport
+	}
+	req, err := http.NewRequest("GET", request.Url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("get url failed:%v", err)
+	}
+	if len(request.Cookie) > 0 {
+		req.Header.Set("Cookie", request.Cookie)
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	bodyReader := bufio.NewReader(resp.Body)
+	e := format.DeterminEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return ioutil.ReadAll(utf8Reader)
+}
---
+++ pkg/format/format.go
@@ -0,0 +1,22 @@
+package format
+
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+)
+
+func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
+
+	bytes, err := r.Peek(1024)
+
+	if err != nil {
+		fmt.Println("fetch error:%v", err)
+		return unicode.UTF8
+	}
+
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
+}
